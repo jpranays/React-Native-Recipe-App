ac2c810ee833362800a7587bf0f4698776319a06
Pranay <pranay1315@gmail.com>
2020-10-09 09:46:25 +0530

    Api config done

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,9 +1,54 @@
-import React from "react";
-import { View, Text } from "react-native";
-function Home() {
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput } from "react-native";
+function Home({ navigation }) {
+	const [query, setQuery] = useState(() => "chicken");
+	const [items, fetchItems] = useState(() => []);
+	const [loadStatus, setLoadingStatus] = useState(() => 0);
+	// useEffect(() => {
+
+	// }, []);
+	useEffect(() => {
+		async function fetchData() {
+			let Api_Key = "4f178338bbff31ecef9be50fad3fc7ad";
+			let Api_ID = "f90763b9";
+			const response = await fetch(
+				`https://api.edamam.com/search?q=${query}&app_id=${Api_ID}&app_key=${Api_Key}`
+			);
+			const data = await response.json();
+			console.log(data);
+			// setQuery("");
+		}
+		fetchData();
+	}, [loadStatus]);
 	return (
-		<View>
-			<Text>Home</Text>
+		<View style={{}}>
+			<View
+				style={{
+					width: "100%",
+					justifyContent: "center",
+					flexDirection: "row",
+					marginTop: 10,
+				}}
+			>
+				<TextInput
+					keyboardType="web-search"
+					onSubmitEditing={() => {
+						setLoadingStatus((prevStatus) => !prevStatus);
+					}}
+					style={{
+						borderBottomWidth: 3,
+						width: "50%",
+						textAlign: "center",
+						fontSize: 20,
+						paddingBottom: 5,
+						borderBottomColor: "green",
+					}}
+					value={query}
+					onChangeText={(value) => {
+						setQuery(value);
+					}}
+				/>
+			</View>
 		</View>
 	);
 }
